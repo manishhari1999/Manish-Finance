64662d55d6fdea2f7633ff6f3315269f3ea2b33a
PURUSHOTHAM REDDY GANGOLLA <manishhari1999@gmail.com>
2023-09-30 17:39:38 +0530

    Finance-Manish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Finance from './components/Finance'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Finance />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
